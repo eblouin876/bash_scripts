b6e98c2054da5af5ce7ed2a61f8e47e50efd0343
Emile Blouin <emile@sreducationgroup.org>
2019-05-31 10:29:30 -0700

    formatting

--- pull.sh
+++ pull.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 BRANCH="$(git branch 2>/dev/null | grep "\*" | colrm 1 2)"
-if [ -n "$BRANCH" ]; then
-if [ -z "$1" ];
-then
-    git pull origin $BRANCH
-else 
-    git pull origin $1
-fi
+if [ -n "$BRANCH" ]; 
+    then
+    if [ -z "$1" ];
+        then
+            git pull origin $BRANCH
+        else 
+            git pull origin $1
+    fi
 fi
--- update.sh
+++ update.sh
@@ -2,43 +2,42 @@
 BRANCH="$(git branch 2>/dev/null | grep "\*" | colrm 1 2)"
 function update() {
 if [ -z "$1" ];
-then
-    if [ $BRANCH == "beta" ];
     then
-    git pull origin beta
-    return
+    if [ $BRANCH == "beta" ];
+        then
+        git pull origin beta
+        return
     fi
     if [ $BRANCH == "master" ];
-    then
-    git pull origin master
-    return
+        then
+        git pull origin master
+        return
     fi
     echo "Would you like to update from beta or master? (b/m)"
     read BR
     if [ $BR == "beta" ] || [ $BR == "b" ];
-    then
-    git pull origin beta
-    return
+        then
+        git pull origin beta
+        return
+    elif [ $BR == "master" ] || [ $BR == "m" ]
+        git pull origin master
+        return
     else
-    git pull origin master
-    return
+        echo "Invalid response. Exiting"
+        return
     fi
 else 
     cd ~/work/$1
-
     if [ $BRANCH == "beta" ];
-    then
-    git pull origin beta
-    return
+        then
+        git pull origin beta
+        return
     fi
     if [ $BRANCH == "master" ];
-    then
-    git pull origin master
-    return
+        then
+        git pull origin master
+        return
     fi
-
-    cd ~/work   
 fi
 }
-
 update
