daa41f3a8857a759ab778be9e305e15c604f98f5
Emile Blouin <emile@sreducationgroup.org>
2019-07-23 14:27:16 -0700

    master: removed useless method

--- bashMethods.sh
+++ bashMethods.sh
@@ -273,18 +273,6 @@ function reload() {
     . ~/.bashrc && . ~/.bash_profile
 }
 
-function size() {
-    if [ "$1" = "-m" ];
-    then
-        echo "$(( $(stat -f%z $2)/1000000 )) mb"
-    elif [ "$1" = "-k" ];
-    then
-        echo "$(( $(stat -f%z $2)/1000 )) kb"
-    else
-        echo "$(( $(stat -f%z $1)/1000 )) kb"
-    fi
-}
-
 function status() {
     git status
 }
